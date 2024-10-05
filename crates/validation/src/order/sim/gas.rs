2277038093f7cf5c8060da8627c2477b85017a7f
Will Smith <williamqsmith11@gmail.com>
2024-10-05 13:28:46 -0500

    make it so inclusive on end pc

--- crates/validation/src/order/sim/gas.rs
+++ crates/validation/src/order/sim/gas.rs
@@ -30,33 +30,44 @@ impl<DB: Database> Inspector<DB> for GasSimulationInspector {
         }
 
         let pc = interp.program_counter();
-        // if  we currently have no measurements. check the next part of the range to
+        // if we currently have no measurements. check the next part of the range to
         // process.
-        if self.in_flight.is_none() {
-            if self.measurement_ranges.contains_key(&pc) {
-                self.in_flight = Some(pc);
-                self.in_flight_start_gas = Some(interp.gas().spent());
-            }
-        } else {
-            // check to see if we have reached the end of this measurement
-            // segment.
-            let end_pc = self
-                .measurement_ranges
-                .get(self.in_flight.as_ref().unwrap())
-                .unwrap();
+        if self.in_flight.is_none() && self.measurement_ranges.contains_key(&pc) {
+            self.in_flight = Some(pc);
+            self.in_flight_start_gas = Some(interp.gas().spent());
+        }
+    }
 
-            // add the results
-            if end_pc == &pc {
-                let start_pc = self.in_flight.take().unwrap();
-                let start_gas = self.in_flight_start_gas.take().unwrap();
+    fn step_end(
+        &mut self,
+        interp: &mut revm::interpreter::Interpreter,
+        context: &mut revm::EvmContext<DB>
+    ) {
+        let addr = interp.contract().bytecode_address.unwrap();
+        if self.in_flight.is_none() && addr != self.angstrom_address {
+            return
+        }
 
-                // ensure we only measure once.
-                self.measurement_ranges.remove(&start_pc);
+        let pc = interp.program_counter();
 
-                let end_gas = interp.gas().spent();
-                let gas_used = end_gas - start_gas;
-                self.results.insert((start_pc, pc), gas_used);
-            }
+        // check to see if we have reached the end of this measurement
+        // segment.
+        let end_pc = self
+            .measurement_ranges
+            .get(self.in_flight.as_ref().unwrap())
+            .unwrap();
+
+        // add the results
+        if end_pc == &pc {
+            let start_pc = self.in_flight.take().unwrap();
+            let start_gas = self.in_flight_start_gas.take().unwrap();
+
+            // ensure we only measure once.
+            self.measurement_ranges.remove(&start_pc);
+
+            let end_gas = interp.gas().spent();
+            let gas_used = end_gas - start_gas;
+            self.results.insert((start_pc, pc), gas_used);
         }
     }
 }
