30a4dd3aed14522e9599afe75469504a322dcdce
Will Smith <williamqsmith11@gmail.com>
2024-10-17 10:20:02 -0500

    add missing impls

--- crates/types/src/reth_db_wrapper.rs
+++ crates/types/src/reth_db_wrapper.rs
@@ -1,6 +1,7 @@
 // Allows us to impl revm::DatabaseRef on the default provider type.
 use reth_provider::{
-    AccountReader, BlockHashReader, StateProofProvider, StateProvider, StateProviderFactory
+    AccountReader, BlockHashReader, BlockIdReader, BlockNumReader, StateProofProvider,
+    StateProvider, StateProviderFactory 
 };
 use reth_storage_api::{StateRootProvider, StorageRootProvider};
 use reth_trie::{
@@ -15,6 +16,142 @@ where
 {
     fn new(db: DB) -> Self {
         Self(db)
+    }
+}
+// impl<DB> BlockHashReader for RethDbWrapper<DB>
+// where
+//     DB: StateProviderFactory + Unpin + Clone + 'static
+// {
+//     fn block_hash(
+//         &self,
+//         number: reth_primitives::BlockNumber
+//     ) -> reth_provider::ProviderResult<Option<reth_primitives::B256>> {
+//         self.0.block_hash(number)
+//     }
+//
+//     fn canonical_hashes_range(
+//         &self,
+//         start: reth_primitives::BlockNumber,
+//         end: reth_primitives::BlockNumber
+//     ) -> reth_provider::ProviderResult<Vec<reth_primitives::B256>> {
+//         self.0.canonical_hashes_range(start, end)
+//     }
+//
+//     fn convert_block_hash(
+//         &self,
+//         hash_or_number: reth_primitives::BlockHashOrNumber
+//     ) -> reth_provider::ProviderResult<Option<reth_primitives::B256>> {
+//         self.0.convert_block_hash(hash_or_number)
+//     }
+// }
+
+impl<DB> BlockNumReader for RethDbWrapper<DB>
+where
+    DB: StateProviderFactory + Unpin + Clone + 'static
+{
+    fn chain_info(&self) -> reth_provider::ProviderResult<ChainInfo> {
+        self.0.chain_info()
+    }
+
+    fn block_number(
+        &self,
+        hash: reth_primitives::B256
+    ) -> reth_provider::ProviderResult<Option<reth_primitives::BlockNumber>> {
+        self.0.block_number(hash)
+    }
+
+    fn convert_number(
+        &self,
+        id: reth_primitives::BlockHashOrNumber
+    ) -> reth_provider::ProviderResult<Option<reth_primitives::B256>> {
+        self.0.convert_block_number(num)
+    }
+
+    fn best_block_number(&self) -> reth_provider::ProviderResult<reth_primitives::BlockNumber> {
+        self.0.best_block_number()
+    }
+
+    fn last_block_number(&self) -> reth_provider::ProviderResult<reth_primitives::BlockNumber> {
+        self.0.last_block_number()
+    }
+
+    fn convert_hash_or_number(
+        &self,
+        id: reth_primitives::BlockHashOrNumber
+    ) -> reth_provider::ProviderResult<Option<reth_primitives::BlockNumber>> {
+        self.0.convert_hash_or_number(id)
+    }
+}
+
+impl<DB> BlockIdReader for RethDbWrapper<DB>
+where
+    DB: StateProviderFactory + Unpin + Clone + 'static
+{
+    fn pending_block_num_hash(&self) -> ProviderResult<Option<reth_primitives::BlockNumHash>> {
+        self.0.pending_block_num_hash()
+    }
+
+    fn safe_block_num_hash(&self) -> ProviderResult<Option<reth_primitives::BlockNumHash>> {
+        self.0.safe_block_num_hash()
+    }
+
+    fn finalized_block_num_hash(&self) -> ProviderResult<Option<reth_primitives::BlockNumHash>> {
+        self.0.finalized_block_num_hash()
+    }
+}
+
+impl<DB> StateProviderFactory for RethDbWrapper<DB>
+where
+    DB: StateProviderFactory + Unpin + Clone + 'static
+{
+    fn latest(&self) -> reth_provider::ProviderResult<reth_provider::StateProviderBox> {
+        self.0.latest()
+    }
+
+    fn pending(&self) -> reth_provider::ProviderResult<reth_provider::StateProviderBox> {
+        self.0.pending()
+    }
+
+    fn state_by_block_id(
+        &self,
+        block_id: reth_primitives::BlockId
+    ) -> reth_provider::ProviderResult<reth_provider::StateProviderBox> {
+        self.0.state_by_block_id(block_id)
+    }
+
+    fn state_by_block_hash(
+        &self,
+        block: reth_primitives::BlockHash
+    ) -> reth_provider::ProviderResult<reth_provider::StateProviderBox> {
+        self.0.state_by_block_hash(block)
+    }
+
+    fn history_by_block_hash(
+        &self,
+        block: reth_primitives::BlockHash
+    ) -> reth_provider::ProviderResult<reth_provider::StateProviderBox> {
+        self.0.history_by_block_hash(block)
+    }
+
+    fn pending_state_by_hash(
+        &self,
+        block_hash: reth_primitives::B256
+    ) -> reth_provider::ProviderResult<Option<reth_provider::StateProviderBox>> {
+        self.0.pending_state_by_hash(block_hash)
+    }
+
+    fn state_by_block_number_or_tag(
+        &self,
+        number_or_tag: reth_primitives::BlockNumberOrTag
+    ) -> reth_provider::ProviderResult<reth_provider::StateProviderBox> {
+        self.0.state_by_block_number_or_tag(number_or_tag)
+    }
+
+    fn history_by_block_number(
+        &self,
+        block: reth_primitives::BlockNumber
+    ) -> reth_provider::ProviderResult<reth_provider::StateProviderBox> {
+        self.0.history_by_block_number(block)
     }
 }
 
