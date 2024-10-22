274c7a03d20e62136832e53e1b6e769320180ae1
Will Smith <williamqsmith11@gmail.com>
2024-10-22 12:33:15 -0500

    wip

--- crates/validation/src/order/state/token_pricing/mod.rs
+++ crates/validation/src/order/state/token_pricing/mod.rs
@@ -3,17 +3,20 @@ use std::{
     sync::Arc
 };
 
-use alloy::primitives::Address;
+use alloy::primitives::{Address, FixedBytes};
 use angstrom_types::{pair_with_price::PairsWithPrice, primitive::PoolId};
 use matching_engine::cfmm::uniswap::pool_manager::UniswapPoolManager;
 
 /// The token price generator gives us the avg instantaneous price of the last 5
 /// blocks of the underlying V4 pool. This is then used in order to convert the
 /// gas used from eth to token0 of the pool the user is swapping over.
+/// In the case of NON direct eth pairs. we assume that any token liquid enough
+/// to trade on angstrom not with eth will always have a eth pair 1 hop away.
+/// this allows for a simple lookup.
 pub struct TokenPriceGenerator<Provider> {
     /// stores the last N amount of prices. TODO: (Address, Address) -> PoolKey
     /// once plamen updates.
-    prev_prices: VecDeque<HashMap<(Address, Address), PairsWithPrice>>,
+    prev_prices: HashMap<FixedBytes<40>, VecDeque<PairsWithPrice>>,
     uni:         Arc<UniswapPoolManager<Provider>>
 }
 
@@ -26,6 +29,10 @@ impl<Provider> TokenPriceGenerator<Provider> {
     ) -> eyre::Result<Self> {
         todo!()
     }
+
+    /// NOTE: assumes that the uniswap pool state transition has already
+    /// occurred.
+    pub fn on_new_block(&mut self) {}
 
     fn get_best_eth_pair(&self, token0: Address, token1: Address) -> PairsWithPrice {
         todo!()
