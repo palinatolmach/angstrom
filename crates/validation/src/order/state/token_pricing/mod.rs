2efbbdcb9f641530594f199e6e0a735b4da04979
Will Smith <williamqsmith11@gmail.com>
2024-10-22 11:10:39 -0500

    w

--- crates/validation/src/order/state/token_pricing/mod.rs
+++ crates/validation/src/order/state/token_pricing/mod.rs
@@ -1,4 +1,20 @@
+use std::collections::{HashMap, VecDeque};
+
+use alloy::primitives::Address;
+use angstrom_types::pair_with_price::PairsWithPrice;
+
 /// The token price generator gives us the avg instantaneous price of the last 5
 /// blocks of the underlying V4 pool. This is then used in order to convert the
 /// gas used from eth to token0 of the pool the user is swapping over.
-pub struct TokenPriceGenerator {}
+pub struct TokenPriceGenerator {
+    /// stores the last N amount of prices
+    prev_prices: VecDeque<HashMap<(Address, Address), PairsWithPrice>>
+}
+
+impl TokenPriceGenerator {
+    /// is a bit of a pain as we need todo a look-back in-order to grab last 5
+    /// blocks.
+    pub async fn new() -> eyre::Result<Self> {
+        todo!()
+    }
+}
